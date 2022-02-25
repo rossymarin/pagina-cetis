eb14fc2ccf1c511d8677f7759ab9fbdd96716ff2
JairoJim <jairojimenezanacleto@gmail.com>
2022-02-24 18:23:57 -0600

    logos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
 
             <img width="250px" className="header_logo ml-50" src="logo-sep.png" alt="header"/>
             <div className="div header_logo">
-                <img className="img" src="portada-halcones.png" alt="header"/>
+                <img className="img" src="portada-halcones2.png" alt="header"/>
             </div>
             <img className="header_logo mr-50" src="logo-dgeti.png" alt="header"/>
 
