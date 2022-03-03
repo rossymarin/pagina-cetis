5ae828e03590a3379f1da6fe603d5e739a18348b
JairoJim <jairojimenezanacleto@gmail.com>
2022-03-03 11:06:13 -0600

    np

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import './Header.css';
-import IconButton from "@material-ui/core/IconButton";
-import ForumIcon from "@material-ui/icons/Forum";
 
 const Header = () => {
     return (
--- src/components/PluginFB.jsx
+++ src/components/PluginFB.jsx
@@ -3,7 +3,7 @@ import './PluginFB.css';
 
 const PluginFB = () => {
     return (
-        <div className="container-plugin">
+        <div className="container-plugin col-12 ">
             <div className="fb-page" data-href="https://www.facebook.com/Cetis28Halcones" data-tabs="timeline" data-width="500px" data-height="600px" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Cetis28Halcones" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Cetis28Halcones">CETis No. 28</a></blockquote></div>
         </div>
     )
