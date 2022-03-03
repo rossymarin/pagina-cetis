7233edd922b5282afe2a1b4e197a9380bded430c
JairoJim <jairojimenezanacleto@gmail.com>
2022-03-03 09:41:43 -0600

    nav

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,10 @@ import './Footer.css';
 const Footer = () => {
     return (
         <footer className="page-footer font-small ">
-            <div className="footer-copyright text-center py-3 footer">
+            <div className="footer-copyright text-center py-2 footer">
                 Centro de Estudios Tecnológicos Industrial y de Servicio N° 28
                 "Ignacio Allende"
+                <p className="text-foot">Km. 108 carr. Fed. No.15 , Zirahuato de los Bernal, Zitácuaro, Mich, C.P.61500</p>
             </div>
         </footer>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <div>
+        <div className="sticky-top">
             <nav className="navbar navbar-expand-lg navbar-light">
                 <a className="navbar-brand" href="#">Inicio</a>
                 <button className="navbar-toggler bg-white" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,7 +19,7 @@ const Navbar = () => {
                             <ul className="dropdown-menu multi-level dropdown-content" role="menu" aria-labelledby="dropdownMenu">
                                 <li><a class="dropdown-item" href="#">CETis 28</a></li>
                                 <div className="dropdown-divider"></div>
-                                <li><a class="dropdown-item" href="#">Orientación Educativa</a></li>
+                                <li><a class="dropdown-item" href="#">Avisos de privacidad</a></li>
                                 <li className="divider"></li>
                                 <li className="dropdown-submenu">
                                     <a class="dropdown-item" tabIndex="-1" href="#">Hover me for more options</a>
@@ -81,7 +81,32 @@ const Navbar = () => {
                                     </ul>
                                 </li>
                             </ul> 
-                        </li>    
+                        </li>
+                        <li className="nav-item dropdown active">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Alumnos
+                            </a>
+                            <ul className="dropdown-menu multi-level dropdown-content" role="menu" aria-labelledby="dropdownMenu">
+                                <li><a href="#">Becas</a></li>
+                                <li><a href="#">Some other action</a></li>
+                                <li className="divider"></li>
+                                <li className="dropdown-submenu">
+                                    <a tabIndex="-1" href="#">Horarios 2022</a>
+                                    <ul className="dropdown-menu">
+                                    <li><a tabIndex="-1" href="#">Second level</a></li>
+                                    <li className="dropdown-submenu">
+                                        <a href="#">Even More..</a>
+                                        <ul className="dropdown-menu">
+                                            <li><a href="#">3rd level</a></li>
+                                            <li><a href="#">3rd level</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Second level</a></li>
+                                    <li><a href="#">Second level</a></li>
+                                    </ul>
+                                </li>
+                            </ul> 
+                        </li>     
                     </ul>
                 </div>
             </nav>
